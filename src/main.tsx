e68e3d24d943ae757fa9259a57436dedc4cdb992
NathanHermes <hermesnathan8@gmail.com>
2026-05-12 14:29:29 -0300

    feat(router): adicionar react-router na aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root")!;
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
